b76bc0f19fd90d5d76be17f3baa9a583419e6109
HannahAmanda <hannah.hansen2@gmail.com>
2015-09-14 12:32:07 +0200

    Fixed if condition in F4 class, mult()

--- DMPD4/src/f4/F4.java
+++ DMPD4/src/f4/F4.java
@@ -7,7 +7,7 @@ package f4;
  *  0 | 0 | 0 | 0 | 0
  *  1 | 0 | 1 | w | w²
  *  w | 0 | w | w²| 1
- *  w²| 0 | w²| 1 | w²
+ *  w²| 0 | w²| 1 | w
  * 
  * _+_|_0_|_1_|_w_|_w²_  
  *  0 | 0 | 1 | w | w²
@@ -25,14 +25,18 @@ public class F4 {
 		
 		if (x.equals(Element.ZERO) || y.equals(Element.ZERO)) {
 			return Element.ZERO;
+			
 		} else if (x.equals(Element.ONE)) {
 			return y;
+		
 		} else if (y.equals(Element.ONE)) {
 			return x;
+		
 		} else if (x.equals(Element.OMEGA)) {
 			
 			if (y.equals(Element.OMEGA)) {
 				return Element.OMEGASQ;
+		
 			} else if (y.equals(Element.OMEGASQ)) {
 				return Element.ONE;
 			}
@@ -41,8 +45,9 @@ public class F4 {
 			
 			if (y.equals(Element.OMEGA)) {
 				return Element.ONE;
+			
 			} else {
-				return Element.OMEGASQ;
+				return Element.OMEGA;
 			}
 		}
 		
